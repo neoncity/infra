565e630fcf0bb6f800e1d842e9b6b517e25070eb
Horia Coman <horia141@gmail.com>
2017-03-10 20:02:00 +0200

    Add a script to pull everything from github.

---
+++ pull-all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd ..
+
+cd causefe
+git pull
+cd ..
+
+cd common-js
+git pull
+cd ..
+
+cd common-server-js
+git pull
+cd ..
+
+cd core
+git pull
+cd ..
+
+cd core-sdk-js
+git pull
+cd ..
+
+cd identity
+git pull
+cd ..
+
+cd identity-sdk-js
+git pull
+cd ..
+
+cd infra
