e0ff67546bbbdd01e06e3b384a01c2c25a47700f
Horia Coman <horia141@gmail.com>
2017-03-25 14:10:52 +0200

    Add a diff-all script.

---
+++ diff-all.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+git diff
+
+cd ..
+
+cd causefe
+git diff
+cd ..
+
+cd common-js
+git diff
+cd ..
+
+cd common-server-js
+git diff
+cd ..
+
+cd core
+git diff
+cd ..
+
+cd core-sdk-js
+git diff
+cd ..
+
+cd identity
+git diff
+cd ..
+
+cd identity-sdk-js
+git diff
+cd ..
+
+cd infra
