72f36c3e0ebed4a11389031ebbd949e5385a8212
Horia Coman <horia141@gmail.com>
2017-03-29 10:14:17 +0300

    Add a comparison between local and remote

--- diff-all.sh
+++ diff-all.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
 
 git diff
+git diff origin/master master
 
 cd ..
 
 cd causefe
 git diff
+git diff origin/master master
 cd ..
 
 cd common-js
 git diff
+git diff origin/master master
 cd ..
 
 cd common-server-js
 git diff
+git diff origin/master master
 cd ..
 
 cd core
 git diff
+git diff origin/master master
 cd ..
 
 cd core-sdk-js
 git diff
+git diff origin/master master
 cd ..
 
 cd identity
 git diff
+git diff origin/master master
 cd ..
 
 cd identity-sdk-js
 git diff
+git diff origin/master master
 cd ..
 
 cd infra
